f0ecc47122dd2bd5c051b8efcab75a2d98f5a3e4
PaphozZ <daniromlist@gmail.com>
2026-03-16 19:04:20 +0300

    подготовка mvvm структуры

---
+++ ImageClassifier.ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ImageClassifier.ViewModel.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        int _count = 0;
+        public ICommand OnCounterClickedCommand { get; }
+
+        private string _buttonText = "Click me";
+
+        public MainViewModel()
+        {
+            OnCounterClickedCommand = new Command(OnCounterClicked);
+        }
+
+        public string ButtonText
+        {  
+            get => _buttonText;
+            set 
+            {
+                _buttonText = value;
+                OnPropertyChanged(nameof(ButtonText));
+            }
+        }
+
+        public void OnCounterClicked()
+        {
+            _count++;
+
+            if (_count == 1)
+                ButtonText = $"Clicked {_count} time";
+            else
+                ButtonText = $"Clicked {_count} times";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ImageClassifier/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using ImageClassifier.ViewModel.ViewModels;
+
+namespace ImageClassifier
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
