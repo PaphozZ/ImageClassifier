017da7ef6e030f72b605f1ba2968dc1fb868a156
PaphozZ <daniromlist@gmail.com>
2026-04-02 10:55:51 +0300

    fix resize window bug

--- ImageClassifier/Views/ImageListView.xaml.cs
+++ ImageClassifier/Views/ImageListView.xaml.cs
@@ -1,9 +1,27 @@
 namespace ImageClassifier.Views;
 
-public partial class ImageListView : ContentView
-{
-	public ImageListView()
-	{
-		InitializeComponent();
-	}
-}
+    public partial class ImageListView : ContentView
+    {
+        private int _currentSpan = -1;
+
+        public ImageListView()
+        {
+            InitializeComponent();
+            PreviewGrid.SizeChanged += OnPreviewGridSizeChanged;
+        }
+
+        private void OnPreviewGridSizeChanged(object? sender, EventArgs e)
+        {
+            if (PreviewGrid.Width <= 0)
+                return;
+
+            int newSpan = (int)(PreviewGrid.Width / 360);
+            if (newSpan < 1) newSpan = 1;
+
+            if (newSpan == _currentSpan)
+                return;
+
+            _currentSpan = newSpan;
+            DynamicGridLayout.Span = newSpan;
+        }
+    }
